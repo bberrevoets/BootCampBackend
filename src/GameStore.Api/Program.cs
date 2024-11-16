039e2105cb170cc21d291d92846dd48376eb64f0
B.N. Berrevoets <bert@berrevoets.net>
2024-11-17 00:36:17 +0100

    Adding the data model.
    Update project structure and add Game model
    
    - Include README.md in Backend.sln under "Solution Items"
    - Update target framework in GameStore.Api.csproj to net9.0
    - Add Game class to GameStore.Api.Models with properties
    - Import Game class in Program.cs and create sample data list
    - Move app.Run() to end of Program.cs and add root endpoint

--- src/GameStore.Api/Program.cs
+++ src/GameStore.Api/Program.cs
@@ -1,6 +1,36 @@
+using GameStore.Api.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 var app = builder.Build();
+
+List<Game> games =
+[
+    new()
+    {
+        Id          = Guid.CreateVersion7(),
+        Name        = "Street Fighter II",
+        Genre       = "Fighting",
+        Price       = 19.99m,
+        ReleaseDate = new(1992, 7, 15)
+    },
+    new()
+    {
+        Id          = Guid.CreateVersion7(),
+        Name        = "Final Fantasy XIV",
+        Genre       = "RolePlaying",
+        Price       = 59.99m,
+        ReleaseDate = new(2010, 9, 30)
+    },
+    new()
+    {
+        Id          = Guid.CreateVersion7(),
+        Name        = "FIFA 23",
+        Genre       = "Sports",
+        Price       = 69.99m,
+        ReleaseDate = new(2022, 9, 27)
+    }
+];
 
 app.MapGet("/", () => "Hello World!");
 
