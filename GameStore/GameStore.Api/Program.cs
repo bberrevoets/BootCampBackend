a784328aae1c6cdcc431e315ceeba41658b3241b
B.N. Berrevoets <bert@berrevoets.net>
2024-12-13 17:20:59 +0100

    Use the HTTP logging middleware
    Add HTTP logging and update configurations
    
    - Updated `Program.cs` to include HTTP logging, removed unused namespaces, and replaced `RequestTimingMiddleware` with `app.UseHttpLogging()`.
    - Configured HTTP logging options to log request paths, methods, and response status codes.
    - Added logging configuration in `appsettings.Development.json` to set log level for `Microsoft.AspNetCore.HttpLogging.HttpLoggingMiddleware` to `None`.
    - Added logging configuration in `appsettings.json` to set log level for `Microsoft.AspNetCore.HttpLogging.HttpLoggingMiddleware` to `Information`.
    - Updated DELETE request URL in `gamestore.http` to a new game ID.

--- GameStore/GameStore.Api/Program.cs
+++ GameStore/GameStore.Api/Program.cs
@@ -1,8 +1,8 @@
-using System.Diagnostics;
 using GameStore.Api.Data;
 using GameStore.Api.Features.Games;
 using GameStore.Api.Features.Genres;
 using GameStore.Api.Shared.Timing;
+using Microsoft.AspNetCore.HttpLogging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -10,12 +10,20 @@ var connString = builder.Configuration.GetConnectionString("GameStore");
 
 builder.Services.AddSqlite<GameStoreContext>(connString);
 
+builder.Services.AddHttpLogging(options =>
+{
+    const HttpLoggingFields flags = HttpLoggingFields.RequestPath | HttpLoggingFields.RequestMethod | HttpLoggingFields.ResponseStatusCode;
+    options.LoggingFields = flags;
+    options.CombineLogs = true;
+});
+
 var app = builder.Build();
 
 app.MapGames();
 app.MapGenres();
 
-app.UseMiddleware<RequestTimingMiddleware>();
+// app.UseMiddleware<RequestTimingMiddleware>();
+app.UseHttpLogging();
 
 await app.InitializeDbAsync();
 
