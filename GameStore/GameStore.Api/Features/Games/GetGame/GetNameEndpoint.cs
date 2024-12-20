7859ad9aabbf20005704edbc99e62c88eb93e37c
B.N. Berrevoets <bert@berrevoets.net>
2024-12-20 15:53:44 +0100

    Catching and logging errors
    Add logging and error handling to MapGet method
    
    Reordered using directives and added System.Diagnostics.
    Enhanced MapGetGame with ILogger<Program> for logging.
    Updated MapGet lambda to include try-catch for error handling.
    Try block attempts to find game by ID, returns NotFound or Ok.
    Catch block logs error details and returns Problem with trace ID.

--- GameStore/GameStore.Api/Features/Games/GetGame/GetNameEndpoint.cs
+++ GameStore/GameStore.Api/Features/Games/GetGame/GetNameEndpoint.cs
@@ -1,7 +1,7 @@
-﻿using GameStore.Api.Data;
+﻿using System.Diagnostics;
+using GameStore.Api.Data;
 using GameStore.Api.Features.Games.Constants;
 using GameStore.Api.Models;
-
 using Microsoft.Data.Sqlite;
 
 namespace GameStore.Api.Features.Games.GetGame;
@@ -10,14 +10,33 @@ public static class GetNameEndpoint
 {
     public static void MapGetGame(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/{id:guid}", async (Guid id, GameStoreContext dbContext) =>
+        app.MapGet("/{id:guid}", async (Guid id, GameStoreContext dbContext, ILogger<Program> logger) =>
         {
-            Game? game = await FindGameAsync(id, dbContext);
+            try
+            {
+                var game = await FindGameAsync(id, dbContext);
 
-            return game is null
-                ? Results.NotFound()
-                : Results.Ok(new GameDetailsDto(game.Id, game.Name, game.GenreId, game.Price, game.ReleaseDate,
-                    game.Description));
+                return game is null
+                    ? Results.NotFound()
+                    : Results.Ok(new GameDetailsDto(game.Id, game.Name, game.GenreId, game.Price, game.ReleaseDate,
+                        game.Description));
+            }
+            catch (Exception ex)
+            {
+                var traceId = Activity.Current?.TraceId;
+
+                logger.LogError(ex, "Could not process a request on machine {Machine}. TraceId: {TraceId}",
+                    Environment.MachineName, traceId);
+
+                return Results.Problem(
+                    title: "An error occured while processing your request.",
+                    statusCode: StatusCodes.Status500InternalServerError,
+                    extensions: new Dictionary<string, object?>
+                    {
+                        { "traceId", traceId.ToString() }
+                    }
+                );
+            }
         }).WithName(EndpointNames.GetGame);
     }
 
