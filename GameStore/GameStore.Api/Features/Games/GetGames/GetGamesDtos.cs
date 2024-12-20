a7e24bfe4b75ae4d2d6d82ed3e3a363275ed19ab
B.N. Berrevoets <bert@berrevoets.net>
2024-12-20 16:22:06 +0100

    Adding DTOs for pagination
    Add DTOs for paginated game data and update HTTP request
    
    Introduced new DTOs for handling paginated game data:
    - `GetGamesDto` with default pagination values.
    - `GamesPageDto` for paginated responses, including total pages and game summaries.
    - `GameSummeryDto` for concise game details.
    
    Updated `gamestore.http` to include pagination query parameters (`pageNumber=1` and `pageSize=3`).

--- GameStore/GameStore.Api/Features/Games/GetGames/GetGamesDtos.cs
+++ GameStore/GameStore.Api/Features/Games/GetGames/GetGamesDtos.cs
@@ -1,3 +1,7 @@
 ﻿namespace GameStore.Api.Features.Games.GetGames;
 
+public record GetGamesDto(int PageNumber = 1, int PageSize = 5);
+
+public record GamesPageDto(int TotalPages, IEnumerable<GameSummeryDto> Data);
+
 public record GameSummeryDto(Guid Id, string Name, string Genre, decimal Price, DateOnly ReleaseDate);
