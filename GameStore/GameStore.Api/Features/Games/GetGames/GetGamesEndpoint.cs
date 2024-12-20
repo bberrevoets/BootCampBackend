77adc80a079c29065782c6d97ed5e4f51e276165
B.N. Berrevoets <bert@berrevoets.net>
2024-12-20 16:35:53 +0100

    Implementing pagination
    Add pagination support to GetGamesEndpoint
    
    Modified GetGamesEndpoint to support pagination by accepting GetGamesDto with PageNumber and PageSize properties. Updated query logic to order by game name, skip records based on page number, and take records based on page size. Response now includes GamesPageDto with total pages and current page games. Updated gamestore.http to change sample GET request page size from 3 to 5.

--- GameStore/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
+++ GameStore/GameStore.Api/Features/Games/GetGames/GetGamesEndpoint.cs
@@ -7,14 +7,28 @@ public static class GetGamesEndpoint
 {
     public static void MapGetGames(this IEndpointRouteBuilder app)
     {
-        app.MapGet("/", async (GameStoreContext dbContext) => await dbContext.Games
-            .Include(game => game.Genre)
-            .Select(game => new GameSummeryDto(
-                game.Id,
-                game.Name,
-                game.Genre!.Name,
-                game.Price,
-                game.ReleaseDate))
-            .AsNoTracking().ToListAsync());
+        app.MapGet("/", async (
+            GameStoreContext dbContext,
+            [AsParameters] GetGamesDto request) =>
+        {
+            var skipCount = (request.PageNumber - 1) * request.PageSize;
+
+            var gamesOnPage = await dbContext.Games
+                .OrderBy(game => game.Name)
+                .Skip(skipCount)
+                .Take(request.PageSize)
+                .Include(game => game.Genre)
+                .Select(game => new GameSummeryDto(
+                    game.Id,
+                    game.Name,
+                    game.Genre!.Name,
+                    game.Price,
+                    game.ReleaseDate))
+                .AsNoTracking().ToListAsync();
+            var totalGames = await dbContext.Games.CountAsync();
+            var totalPages = (int)Math.Ceiling(totalGames / (double)request.PageSize);
+
+            return new GamesPageDto(totalPages, gamesOnPage);
+        });
     }
 }
